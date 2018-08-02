ffc52a696c84939525da74775b5ec1f859cf5299
Brijesh Rakholiya <brijeshrakholiya6@gmail.com>
2018-08-02 09:10:39 +0530

    Update accept.php

--- Online-delivery/accept.php
+++ Online-delivery/accept.php
@@ -1,9 +1,21 @@
 <?php
 $id=$_GET['order'];
+
+
 echo $id;
+
+
 $con = mysql_connect("localhost","jjm","123");
+
+
 mysql_select_db("restraunt", $con);
+
+
 $sql="DELETE from restraunt.order WHERE id  = $id";
+
+
 mysql_query($sql,$con);
+
+
 header("Location: view_orders.php");
 ?>
